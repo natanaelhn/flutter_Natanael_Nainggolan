d0d3457663cfc7e3fe583bcc7caab5e186c6f898
natanaelhn <nainggolannainggolan74@gmail.com>
2022-08-29 23:01:22 +0700

    mengerjakan tugas bab Introduction to Algorithm and Dart Programming

---
+++ 4_Introduction to Algorithm and Dart Programming/praktikum/luas_lingkaran.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+
+  int inputJari = 7;
+  print("Jari jari = $inputJari");
+
+  double luasLingkaran = (22/7) * (inputJari * inputJari);
+
+  print("Luas lingkaran = $luasLingkaran");
+}
---
+++ 4_Introduction to Algorithm and Dart Programming/praktikum/tiga_variabel.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+
+  String var1 = "Natanael";
+  String var2 = "Halomoan";
+  String var3 = "Nainggolan";
+
+  print("$var1 $var2 $var3");
+}
